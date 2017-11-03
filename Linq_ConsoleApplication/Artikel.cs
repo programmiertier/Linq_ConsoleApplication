6175ff6ddc91a8d89b5ff02d1ec853935cb13a19
programmiertier <zimmermannveronique@protonmail.com>
2017-11-03 11:27:52 +0100

    Yoda-SQL
    Der erste Versuch

---
+++ Linq_ConsoleApplication/Artikel.cs
@@ -0,0 +1,53 @@
+﻿internal class Artikel
+    {
+        private int _artikel_id;
+        private double _artikel_volumen;
+        private double _artikel_preis;
+
+    public double artikel_preis
+    {
+        get
+        {
+            return _artikel_preis;
+        }
+    }
+
+    public double artikel_volumen
+    {
+        get
+        {
+            return _artikel_volumen;
+        }
+    }
+
+    public int artikel_id
+    {
+        get
+        {
+            return _artikel_id;
+        }
+    }
+
+
+
+    public Artikel()
+    {
+            //  WriteLine("Artikel  angelegt:");
+    }
+
+    public Artikel(int id, Artikel[] wkatalog)
+    { // WriteLine("Artikel {0} angelegt: ", wk[id]._art_id);
+        _artikel_id = wkatalog[id]._artikel_id;
+        _artikel_volumen = wkatalog[id]._artikel_volumen;//1.0/((double)id*3);
+        _artikel_preis = wkatalog[id]._artikel_preis; // 1.0 / ((double) id * 5) ;
+            //  WriteLine("Der Artikel hat ein Volumen von {0:F2} und einen Preis von {1:F2}", _art_volumen, _art_einzelpreis);
+    }
+
+    public Artikel(int id, double volumen, double preis)
+    {
+        // WriteLine("Artikel {0} angelegt: ", id);
+        _artikel_id = id;
+        _artikel_volumen = volumen;
+        _artikel_preis = preis;
+    }
+}
--- Linq_ConsoleApplication/Program.cs
+++ Linq_ConsoleApplication/Program.cs
@@ -1,15 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Linq;      // diese library brauchen wir
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace Linq_ConsoleApplication
 {
     class Program
     {
+        static void warten()
+        {
+            WriteLine("-----");
+            WriteLine("Weiter mit Enter");
+            ReadLine();
+        }
+
+        static int verdoppeln(int zahl)
+        {
+            return zahl * 2;
+        }
+
         static void Main(string[] args)
         {
+            /* Warenkatalog wkatalog = new Warenkatalog();
+            // foreach ( Artikel item in wkatalog)     braucht immer Enumerable Daten
+            for (int id = 0; id < Warenkatalog.warenkatalog.Length; id++)
+            {
+                WriteLine("Artikel: {0},\tVolumen: {1},\tPreis: {2}", 
+                    Warenkatalog.warenkatalog[id].artikel_id, 
+                    Warenkatalog.warenkatalog[id].artikel_volumen, 
+                    Warenkatalog.warenkatalog[id].artikel_preis);
+            }   */
+
+            // in SQL SELECT * FROM Warenkatalog.warenkatalog -> alle Inhalte Spalten und Zeilen
+            //                      Quelle / Table
+            /* var artikelliste = from artikel in Warenkatalog.warenkatalog select artikel;    // in LINQ Syntax
+                  // from = Bereichsangabe // name beliebig!
+            foreach(var item in artikelliste)
+            {
+                WriteLine("Artikel: {0},\tVolumen: {1},\tPreis: {2}", 
+                    item.artikel_id, 
+                    item.artikel_volumen, 
+                    item.artikel_preis);
+            }
+            warten();
+
+            // in SQL: select volumen from Warenkatalog.warenkatalog
+            var artikelvolumen = from artikel in Warenkatalog.warenkatalog select artikel.artikel_volumen;
+            foreach (var item in artikelvolumen)
+            {
+                WriteLine("Volumen: {0}", item);
+            }
+            warten();
+
+            var artikelpreis = from artikel in Warenkatalog.warenkatalog select artikel.artikel_preis;
+            foreach (var item in artikelpreis)
+            {
+                WriteLine("Preis: {0}", item);
+            }
+            warten();   */
+
+            // Select mit berechnetem Ergebnis aus einer Spalte
+            var artikelImRegal = from artikel in Warenkatalog.warenkatalog select (int)(1.0 / artikel.artikel_volumen);
+            foreach ( var item in artikelImRegal)
+            {
+                WriteLine("Artikel Im Regal: {0}", item);       // entweder hier (int)item oder nach dem select (int)()
+            }
+            warten();
+
+            // Methode im LINQ aufgerufen
+            var doppelteImRegal = from artikel in Warenkatalog.warenkatalog select verdoppeln((int)(1.0 / artikel.artikel_volumen));
+            foreach( var item in doppelteImRegal)
+            {
+                WriteLine("Doppeltes im Regal: {0}", item);
+            }
+            warten();
+
+            // Select mit berechnetem Ergebnis aus mehreren Spalten
+            var regalWert = from artikel in Warenkatalog.warenkatalog select ((int)(1.0 / artikel.artikel_volumen) * artikel.artikel_preis);
+            foreach (var item in regalWert)
+            {
+                WriteLine("Wert im Regal: {0}", item);       // entweder hier (int)item oder nach dem select (int)()
+            }
+            warten();
+
+            // Select mit berechnetem Ergebnis aus mehreren  Spalten  und einer Spalte, wie AS in SQL
+            var idUndRegalwert = from artikel in Warenkatalog.warenkatalog select new { id = artikel.artikel_id,
+                                                                                        wert = ((int)(1.0 / artikel.artikel_volumen)) * artikel.artikel_preis };    // in LINQ Syntax
+            foreach (var item in idUndRegalwert)
+            {
+                WriteLine("Regal: {0} hat den Wert: {1,4:F2}", item.id, item.wert);
+            }
+            warten();
         }
     }
 }
